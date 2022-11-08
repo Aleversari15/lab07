e079f74a097a9141dee88d8d52e07fa37a5510b3
Alessandra Versari - alessandra.versari2@studio.unibo.it <alessandra.versari2@studio.unibo.it>
2022-11-08 15:52:57 +0100

    finishing exercise 61

--- 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,5 +1,7 @@
 package it.unibo.nestedenum;
 
+import java.lang.reflect.AnnotatedTypeVariable;
+import java.lang.reflect.MalformedParameterizedTypeException;
 import java.util.Comparator;
 import java.util.Locale;
 import java.util.Objects;
@@ -18,4 +20,31 @@ public final class MonthSorterNested implements MonthSorter {
     public Comparator<String> sortByOrder() {
         return null;
     }
+
+    public enum Month {
+        GENNAIO("Gennaio",31),
+        FEBBRAIO("Febbraio",28),
+        MARZO("Marzo",31),
+        APRILE("Aprile",30),
+        MAGGIO("Maggio",31),
+        GIUGNO("Giugno",30),
+        LUGLIO("Luglio",31),
+        AGOSTO("Agosto",31),
+        SETTEMBRE("Settembre",30),
+        OTTOBRE("Ottobre",31),
+        NOVEMBRE("Novembre",30),
+        DICEMBRE("Dicembre",31);
+
+        private final String name;
+        private final int days;
+
+        private Month(final String name,final int days){
+            this.name=name;
+            this.days=days;
+        }
+
+        public Month fromString(final String stringa){
+
+        }
+   }
 }
